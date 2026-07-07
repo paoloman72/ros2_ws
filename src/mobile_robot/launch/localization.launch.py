9479377407ec80075e7a45d6a800cb0bc2b363d3
paoloman <p.maninetti@reply.com>
2026-07-07 15:19:58 +0200

    added localization

---
+++ src/mobile_robot/launch/localization.launch.py
@@ -0,0 +1,42 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    pkg_share = get_package_share_directory("mobile_robot")
+
+    params_file = os.path.join(
+        pkg_share,
+        "config",
+        "localization.yaml"
+    )
+
+    return LaunchDescription([
+        Node(
+            package="nav2_map_server",
+            executable="map_server",
+            name="map_server",
+            output="screen",
+            parameters=[params_file],
+        ),
+
+        Node(
+            package="nav2_amcl",
+            executable="amcl",
+            name="amcl",
+            output="screen",
+            parameters=[params_file],
+        ),
+
+        Node(
+            package="nav2_lifecycle_manager",
+            executable="lifecycle_manager",
+            name="lifecycle_manager_localization",
+            output="screen",
+            parameters=[params_file],
+        ),
+    ])
